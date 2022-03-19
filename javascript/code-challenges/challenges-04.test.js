95cb7d2c2b06af11adc418d16e45b4df588f068d
Michael Maker <maker.mdm@gmail.com>
2022-03-18 23:01:34 -0500

    Start challenge 6

--- javascript/code-challenges/challenges-04.test.js
+++ javascript/code-challenges/challenges-04.test.js
@@ -103,9 +103,8 @@ Here is an example of the input:
 ];
 ------------------------------------------------------------------------------------------------ */
 
-const sortByPrice = (arr) => {
-  // Solution code here...
-};
+// a & b represent each object here
+const sortByPrice = (arr) => arr.sort((a, b) => a.price - b.price);
 
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 7 - Stretch Goal
@@ -175,6 +174,16 @@ If two people have the same full name, the younger one should come first. Do not
 
 const sortPeopleBetter = (arr) => {
   // Solution code here...
+  return arr.sort((a, b) => {
+    if (a.lastName.toLowerCase() !== b.lastName.toLowerCase()) {
+      return a.lastName > b.lastName ? 1 : -1;
+    } else if (a.firstName.toLowerCase() !== b.firstName.toLowerCase()); {
+      return a.firstName > b.firstName ? 1 : -1;
+    }
+    {else {
+      return a.age - b.age;
+    }
+    });
 };
 
 /* ------------------------------------------------------------------------------------------------
